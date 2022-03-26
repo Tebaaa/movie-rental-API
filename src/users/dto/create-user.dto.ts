3a8c57d1cdc599eb7fb4aac7abf83470fd5cea38
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-25 23:46:45 -0300

    Add DTOs to create and update user

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsString } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  readonly username: string;
+
+  @IsString()
+  readonly password: string;
+
+  @IsString()
+  readonly role: string;
+}

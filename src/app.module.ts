308d84778f9b50684b5984149340fb6385964b78
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-26 01:03:49 -0300

    Add TypeOrm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, MoviesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'teba',
+      password: 'admin',
+      database: 'movies_rental',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    MoviesModule,
+  ],
 })
 export class AppModule {}

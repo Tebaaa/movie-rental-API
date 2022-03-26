833931dc9b005394dd9dfff2e036734fb3f6c8b6
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-26 01:42:31 -0300

    Add User entity

---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,17 @@
+import { Exclude } from 'class-transformer';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Exclude()
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+}

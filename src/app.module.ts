2130f914fd10612ea484897a3d5089d7c78422f7
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-26 00:06:50 -0300

    Add movies module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MoviesModule],
 })
 export class AppModule {}

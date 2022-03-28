a2b146e7adeabd28f488081c18618385a65a366f
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 20:01:59 -0300

    Add endpoint to buy, rent or return movies

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -11,14 +12,20 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/strategies/jwt/jwt-auth.guard';
+import { ActionDto } from './dto/action.dto';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { AdminGuard } from './guards/admin.guard';
-import { MoviesService } from './movies.service';
+import { ClientGuard } from './guards/client.guard';
+import { MoviesService } from './services/movies.service';
+import { RentalServices } from './services/rental-services.service';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+    private rentalService: RentalServices,
+  ) {}
 
   @Get()
   getAll() {
@@ -47,5 +54,24 @@ export class MoviesController {
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id') id: number) {
     return this.moviesService.delete(id);
+  }
+
+  @UseGuards(JwtAuthGuard, ClientGuard)
+  @Post(':id')
+  rentalAction(@Param('id') id: number, @Body() actionDto: ActionDto) {
+    const actionIsBuy = actionDto.action === 'buy';
+    const actionIsRent = actionDto.action === 'rent';
+    const actionIsReturn = actionDto.action === 'return';
+
+    switch (true) {
+      case actionIsBuy:
+        return this.rentalService.buy(id);
+      case actionIsRent:
+        return this.rentalService.rent(id);
+      case actionIsReturn:
+        return this.rentalService.return(id);
+      default:
+        throw new BadRequestException('Value must be buy, rent or return');
+    }
   }
 }

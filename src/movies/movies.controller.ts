cba4ccd8bf4232d160d1e8b8095af558c64deb51
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 15:08:34 -0300

    Use jwt and admin guards

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,9 +8,12 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt/jwt-auth.guard';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { AdminGuard } from './guards/admin.guard';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
@@ -27,16 +30,19 @@ export class MoviesController {
     return this.moviesService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @Post()
   create(@Body() createMovieDto: CreateMovieDto) {
     return this.moviesService.create(createMovieDto);
   }
 
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @Patch(':id')
   update(@Body() updateMovieDto: UpdateMovieDto, @Param('id') id: number) {
     return this.moviesService.update(id, updateMovieDto);
   }
 
+  @UseGuards(JwtAuthGuard, AdminGuard)
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id') id: number) {

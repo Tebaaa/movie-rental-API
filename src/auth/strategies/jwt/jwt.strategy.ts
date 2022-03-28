35855982ef6a526348a5e5e3580a7e9822dae366
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 12:13:27 -0300

    Implement jwt strategy

---
+++ src/auth/strategies/jwt/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secret',
+    });
+  }
+
+  //Acá puedo poner más código para intentar hacer un logout?
+  async validate(payload) {
+    return { id: payload.id, username: payload.username };
+  }
+}

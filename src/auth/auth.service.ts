08eace1cbdebbf98555ea56bbc17fc0fce725f2f
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 12:34:00 -0300

    Create service and controller for 'auth/login'

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.findByUsername(username);
+    if (user && user.password === password) {
+      return user;
+    }
+    return null;
+  }
+  async login(user) {
+    const payload = { username: user.username, id: user.id };
+    return { acces_token: this.jwtService.sign(payload) };
+  }
+}

62f06be4ee718ea2eb9c1e52b7096ba160d77058
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 15:03:08 -0300

    Add roles guards

---
+++ src/movies/guards/client.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { RoleChecker } from './roleChecker';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roleChecker = new RoleChecker(context);
+    if (roleChecker.isClient()) return true;
+    return false;
+  }
+}

90a2f379e18195331208fcdeef3b9d5d6e4b9ab9
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 12:07:33 -0300

    Import Type Orm for feature movieEntity

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieEntity } from './entities/movie.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [MoviesController],
-  providers: [MoviesService]
+  providers: [MoviesService],
 })
 export class MoviesModule {}

2ed7ffaa52b4b06250b1e5903c6742ca91d09bf5
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-28 20:03:44 -0300

    Add RentalServices provider

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieEntity } from './entities/movie.entity';
 import { MoviesController } from './movies.controller';
-import { MoviesService } from './movies.service';
+import { MoviesService } from './services/movies.service';
+import { RentalServices } from './services/rental-services.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [MoviesController],
-  providers: [MoviesService],
+  providers: [MoviesService, RentalServices],
 })
 export class MoviesModule {}

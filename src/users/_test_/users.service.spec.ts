e5b1fa97f40244dfeeaf70450151b9382289bc15
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-03-29 18:39:05 -0300

    Import MockRepository and createMockRepository

--- src/users/_test_/users.service.spec.ts
+++ src/users/_test_/users.service.spec.ts
@@ -1,19 +1,13 @@
 import { ConflictException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection, Repository } from 'typeorm';
+import { Connection } from 'typeorm';
+import {
+  createMockRepository,
+  MockRepository,
+} from '../../create-mock-repository.class';
 import { User } from '../entities/users.entity';
 import { UsersService } from '../users.service';
-
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
-const createMockRepository = <T = any>(): MockRepository<T> => ({
-  findOne: jest.fn(),
-  create: jest.fn(),
-  remove: jest.fn(),
-  find: jest.fn(),
-  preload: jest.fn(),
-  save: jest.fn(),
-});
 
 describe('UsersService', () => {
   let service: UsersService;

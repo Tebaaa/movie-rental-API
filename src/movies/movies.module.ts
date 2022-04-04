71732c429fa1ec09f5e193dcc7a1d80bbdde2097
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-04 11:18:11 -0400

    Add tag entity to TypeOrmModule

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieEntity } from './entities/movie.entity';
+import { TagEntity } from './entities/tags.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './services/movies.service';
 import { RentalServices } from './services/rental-services.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieEntity])],
+  imports: [TypeOrmModule.forFeature([MovieEntity, TagEntity])],
   controllers: [MoviesController],
   providers: [MoviesService, RentalServices],
 })

7691bbd95ba68838ef5ecf1d29373fbec19680e9
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-04 10:58:04 -0400

    Retrieve movies with tags

--- src/movies/services/movies.service.ts
+++ src/movies/services/movies.service.ts
@@ -8,18 +8,26 @@ import { Repository } from 'typeorm';
 import { CreateMovieDto } from '../dto/create-movie.dto';
 import { UpdateMovieDto } from '../dto/update-movie.dto';
 import { MovieEntity } from '../entities/movie.entity';
+import { TagEntity } from '../entities/tags.entity';
 
 @Injectable()
 export class MoviesService {
   constructor(
     @InjectRepository(MovieEntity)
     private readonly movieRepository: Repository<MovieEntity>,
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
   ) {}
   findAll() {
-    return this.movieRepository.find({ order: { title: 'ASC' } });
+    return this.movieRepository.find({
+      order: { title: 'ASC' },
+      relations: ['tags'],
+    });
   }
   async findById(id: number) {
-    const movie = await this.movieRepository.findOne(id);
+    const movie = await this.movieRepository.findOne(id, {
+      relations: ['tags'],
+    });
     if (!movie) throw new NotFoundException(`Movie #${id} not found`);
     return movie;
   }
@@ -28,17 +36,39 @@ export class MoviesService {
       title: createMovieDto.title,
       description: createMovieDto.description,
     });
-    if (existingMovie) throw new ConflictException('Movie already exists');
-    const movie = this.movieRepository.create(createMovieDto);
+    if (existingMovie) {
+      throw new ConflictException('Movie already exists');
+    }
+    const tags = await Promise.all(
+      createMovieDto.tags.map((name) => this.preloadTagByName(name)),
+    );
+    const movie = this.movieRepository.create({ ...createMovieDto, tags });
     return this.movieRepository.save(movie);
   }
   async update(id: number, updateMovieDto: UpdateMovieDto) {
-    const movie = await this.movieRepository.preload({ id, ...updateMovieDto });
+    const tags =
+      updateMovieDto.tags &&
+      (await Promise.all(
+        updateMovieDto.tags.map((name) => this.preloadTagByName(name)),
+      ));
+    const movie = await this.movieRepository.preload({
+      id,
+      ...updateMovieDto,
+      tags,
+    });
     if (!movie) throw new NotFoundException(`Movie #${id} not found`);
     return this.movieRepository.save(movie);
   }
   async delete(id: number) {
     const movie = await this.findById(id);
     return this.movieRepository.remove(movie);
+  }
+
+  private async preloadTagByName(name: string) {
+    const existingTag = await this.tagRepository.findOne({ name });
+    if (existingTag) {
+      return existingTag;
+    }
+    throw new NotFoundException(`Tag '${name}' doesn't exist`);
   }
 }

4346a8ed5d0c224f97a360415b7bf886355ffc57
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 13:07:23 -0400

    Add name and lastname fields to user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,24 @@
-import { IsBoolean, IsEmail, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 export class CreateUserDto {
   @IsEmail()
   readonly email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  readonly lastname: string;
+
+  @IsNotEmpty()
   @IsString()
   readonly password: string;
 

c0872de564d5a07e8bdb2ed3fcc8fa93a00facb9
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 17:13:02 -0400

    Create OrderInfo constructor

---
+++ src/movie-rental/classes/order-info.class.ts
@@ -0,0 +1,46 @@
+import { MovieEntity } from '../../movies/entities/movie.entity';
+import { User } from '../../users/entities/users.entity';
+import { IOrderInfo } from '../interfaces/order-info.interface';
+
+export class OrderInfo implements IOrderInfo {
+  quantity: number;
+  email: string;
+  action: 'bought' | 'rented';
+  movies_info: string[];
+  total: number;
+  name: string;
+
+  constructor(movies: MovieEntity[], user: User, action: 'bought' | 'rented') {
+    this.action = action;
+    this.name = user.name;
+    this.email = user.email;
+    this.quantity = movies.length;
+    if (action === 'bought') {
+      this.total = movies.reduce((amount, movie) => {
+        amount += movie.sale_price;
+        return amount;
+      }, 0);
+      this.movies_info = movies.map((movie) => {
+        const movie_info = {
+          title: movie.name,
+          description: movie.description,
+          price: movie.sale_price,
+        };
+        return JSON.stringify(movie_info);
+      });
+    } else {
+      this.total = movies.reduce((amount, movie) => {
+        amount += movie.rent_price;
+        return amount;
+      }, 0);
+      this.movies_info = movies.map((movie) => {
+        const movie_info = {
+          title: movie.name,
+          description: movie.description,
+          price: movie.rent_price,
+        };
+        return JSON.stringify(movie_info);
+      });
+    }
+  }
+}

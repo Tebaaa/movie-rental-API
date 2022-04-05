4531fe26b9796bae0505e0ee638ce11b422281ca
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 16:23:46 -0400

    Add User and MovieEntity typeOrm features

--- src/movie-rental/movie-rental.module.ts
+++ src/movie-rental/movie-rental.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieEntity } from '../movies/entities/movie.entity';
 import { RecordEntity } from '../users/entities/record.entity';
+import { User } from '../users/entities/users.entity';
 import { MovieRentalService } from './movie-rental.service';
 
 @Module({
   providers: [MovieRentalService],
-  imports: [TypeOrmModule.forFeature([RecordEntity])],
+  imports: [TypeOrmModule.forFeature([RecordEntity, User, MovieEntity])],
   exports: [MovieRentalService],
 })
 export class MovieRentalModule {}

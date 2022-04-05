5d1010b835fc9d19ef6b3dfc72b338392a248a8b
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 16:19:41 -0400

    Add sendOrderInfo method to mail service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,16 +1,17 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { MovieEntity } from '../movies/entities/movie.entity';
 import { User } from '../users/entities/users.entity';
 
 @Injectable()
 export class MailService {
   constructor(private mailerService: MailerService) {}
+  host = process.env.SERVER_HOST;
+  port = process.env.SERVER_PORT;
 
   async sendPasswordReset(user: User) {
-    const host = process.env.SERVER_HOST;
-    const port = process.env.SERVER_PORT;
     const userId = user.id;
-    const url = `${host}:${port}/account/password?=id=${userId}`;
+    const url = `${this.host}:${this.port}/account/password?=id=${userId}`;
 
     await this.mailerService.sendMail({
       to: user.email,
@@ -19,6 +20,29 @@ export class MailService {
       context: {
         url,
         url2: url,
+      },
+    });
+  }
+
+  async sendOrderInfo(
+    total: number,
+    movies: MovieEntity[],
+    user: User,
+    action: 'bought' | 'rented',
+    movies_info,
+  ) {
+    const quantity = movies.length;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      subject: 'Order Info',
+      template: 'send-order-info',
+      context: {
+        name: user.name,
+        action,
+        quantity,
+        movies_info: movies_info,
+        total,
       },
     });
   }

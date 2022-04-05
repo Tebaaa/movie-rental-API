20c7acd09e3c5ca8b71157d4733c5ce0e1f14927
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 13:08:24 -0400

    Create record entity

---
+++ src/users/entities/record.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { MovieEntity } from '../../movies/entities/movie.entity';
+import { User } from './users.entity';
+
+@Entity('record')
+export class RecordEntity {
+  @PrimaryGeneratedColumn()
+  record_id: number;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  movie_id: number;
+
+  @Column({ nullable: true })
+  rented: boolean;
+
+  @Column({ nullable: true })
+  buy: boolean;
+
+  @Column({ nullable: true })
+  return: boolean;
+
+  @ManyToOne((type) => User, (user) => user.record)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne((type) => MovieEntity, (movie) => movie.record)
+  @JoinColumn({ name: 'movie_id' })
+  movie: MovieEntity;
+}

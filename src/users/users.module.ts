417bc0973f23d3e95346add5507aa1a391f13cb0
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 13:16:00 -0400

    Import MovieRentalModule & add RecordEntity feature

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieRentalModule } from '../movie-rental/movie-rental.module';
+import { RecordEntity } from './entities/record.entity';
 import { User } from './entities/users.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, RecordEntity]), MovieRentalModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],

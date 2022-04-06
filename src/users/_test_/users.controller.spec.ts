d7f89dc8fc6b4425cbdbc8fa3505b6dc3e15860c
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-06 12:20:51 -0400

    Import users service mock from mocks directory

--- src/users/_test_/users.controller.spec.ts
+++ src/users/_test_/users.controller.spec.ts
@@ -1,36 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovieRentalService } from '../../movie-rental/movie-rental.service';
+import {
+  mockIdDto,
+  mockUserService,
+  mockUsersList,
+} from '../../_test_mocks_/user-service.mock';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { IdDto } from '../dto/id.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
-import { User } from '../entities/users.entity';
 import { UsersController } from '../users.controller';
 import { UsersService } from '../users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
-  const mockIdDto: IdDto = { id: '2d0ef3e8-c253-4c6a-97b7-fbf8953ce0a2' };
-  const mockUser: User = {
-    id: '2d0ef3e8-c253-4c6a-97b7-fbf8953ce0a2',
-    email: 'email@example.com',
-    password: 'password',
-    name: 'name',
-    admin: true,
-    client: true,
-    lastname: 'lastname',
-    record: [],
-  };
-  const mockUsersList: User[] = [
-    { ...mockUser },
-    {
-      ...mockUser,
-      id: '2d0ef3e8-c253-4c6a-97b7-fbf8953ce0a3',
-    },
-    {
-      ...mockUser,
-      id: '2d0ef3e8-c253-4c6a-97b7-fbf8953ce0a4',
-    },
-  ];
   const mockCreateUserDto: CreateUserDto = {
     email: 'newEmail@mail.com',
     password: 'new pass',
@@ -39,27 +20,6 @@ describe('UsersController', () => {
     name: 'new user name',
   };
   const mockUpdateUserDto: UpdateUserDto = { name: 'updated name' };
-  const mockUserService = {
-    findAll: jest.fn(() => ['test']),
-    findById: jest.fn((idDto: IdDto): User => {
-      return mockUsersList.find((user) => user.id === idDto.id);
-    }),
-    create: jest.fn((dto): User => {
-      return {
-        id: '2d0ef3e8-c253-4c6a-97b7-fbf8953ce0a8',
-        ...dto,
-      };
-    }),
-    update: jest.fn((id: number, dto): User => {
-      return {
-        id,
-        ...dto,
-      };
-    }),
-    delete: jest.fn((idDto: IdDto): User => {
-      return mockUsersList.find((user) => user.id === idDto.id);
-    }),
-  };
 
   const mockMovieRentalService = {};
 

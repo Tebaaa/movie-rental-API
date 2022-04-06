9a6934b6a0f3168f8dffe9f2a10828bb9edd4b53
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-06 11:19:48 -0400

    Add swagger cli plugin to document endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +17,32 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Movie Rental API')
+    .setDescription(
+      'API that handle movies requests. It shows a list of movies or a single movie if ID is provided.' +
+        ' Also, if user has a client role, they can buy, rent or return a movie. ' +
+        'If a user is admin, they can manage/create movies and other users.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', bearerFormat: 'Token', scheme: 'bearer' },
+      'access-token',
+    )
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+  SwaggerModule.setup('docs', app, document, customOptions);
+
   await app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST);
 }
 bootstrap();

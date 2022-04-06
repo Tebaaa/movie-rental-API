f61a1d882fff71d87e5017e148114d6e6751455d
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 23:15:12 -0400

    Remove unused lines

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -13,21 +12,15 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/strategies/jwt/jwt-auth.guard';
-import { ActionDto } from './dto/action.dto';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { QueryParamsDto } from './dto/query-params.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { AdminGuard } from './guards/admin.guard';
-import { ClientGuard } from './guards/client.guard';
 import { MoviesService } from './movies.service';
-import { RentalServices } from './rental-services.service';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(
-    private moviesService: MoviesService,
-    private rentalService: RentalServices,
-  ) {}
+  constructor(private moviesService: MoviesService) {}
 
   @Get()
   getAll(@Query() query: QueryParamsDto) {
@@ -56,24 +49,5 @@ export class MoviesController {
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id') id: number) {
     return this.moviesService.delete(id);
-  }
-
-  @UseGuards(JwtAuthGuard, ClientGuard)
-  @Post(':id')
-  rentalAction(@Param('id') id: number, @Body() actionDto: ActionDto) {
-    const actionIsBuy = actionDto.action === 'buy';
-    const actionIsRent = actionDto.action === 'rent';
-    const actionIsReturn = actionDto.action === 'return';
-
-    switch (true) {
-      case actionIsBuy:
-        return this.rentalService.buy(id);
-      case actionIsRent:
-        return this.rentalService.rent(id);
-      case actionIsReturn:
-        return this.rentalService.return(id);
-      default:
-        throw new BadRequestException('Value must be buy, rent or return');
-    }
   }
 }
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -4,12 +4,11 @@ import { MovieEntity } from './entities/movie.entity';
 import { TagEntity } from './entities/tags.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
-import { RentalServices } from './rental-services.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MovieEntity, TagEntity])],
   controllers: [MoviesController],
-  providers: [MoviesService, RentalServices],
+  providers: [MoviesService],
   exports: [MoviesService],
 })
 export class MoviesModule {}

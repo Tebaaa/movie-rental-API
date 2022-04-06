9983d150d3d7f4087bf592bc6aa2688fb5d71cab
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-06 01:09:40 -0400

    Add changePassword endpoint to account module

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -3,11 +3,16 @@ import {
   Controller,
   HttpCode,
   HttpStatus,
+  Param,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/strategies/jwt/jwt-auth.guard';
 import { IdDto } from '../users/dto/id.dto';
+import { CorrectIdGuard } from '../users/guards/correct-id.guard';
 import { AccountService } from './account.service';
+import { ChangePasswordDto } from './dto/change-password.dto';
 import { EmailDto } from './dto/email.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
 
@@ -28,5 +33,15 @@ export class AccountController {
     @Body() resetPasswordDto: ResetPasswordDto,
   ) {
     return this.accountService.resetPassword(idDto, resetPasswordDto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAuthGuard, CorrectIdGuard)
+  @Put('password/:id')
+  changePassword(
+    @Param() idDto: IdDto,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    return this.accountService.changePassword(idDto, changePasswordDto);
   }
 }
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -2,6 +2,7 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { MailService } from '../mail/mail.service';
 import { IdDto } from '../users/dto/id.dto';
 import { UsersService } from '../users/users.service';
+import { ChangePasswordDto } from './dto/change-password.dto';
 import { EmailDto } from './dto/email.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
 
@@ -25,5 +26,27 @@ export class AccountService {
       return this.usersService.update(idDto, updatePassword);
     }
     throw new ConflictException('Passwords must match');
+  }
+
+  async changePassword(idDto: IdDto, changePasswordDto: ChangePasswordDto) {
+    const user = await this.usersService.findById(idDto);
+    const { oldPassword, newPassword, newPasswordConfirmation } =
+      changePasswordDto;
+    const correctOldPassword = user.password === oldPassword;
+    const equalNewPassword = newPassword === newPasswordConfirmation;
+    const oldPasswordEqualNewPassword = newPassword === oldPassword;
+    const updatePassword = { password: newPassword };
+    switch (true) {
+      case !correctOldPassword:
+        throw new ConflictException('Incorrect old password');
+      case !equalNewPassword:
+        throw new ConflictException('Your new password must match');
+      case oldPasswordEqualNewPassword:
+        throw new ConflictException(
+          'Your new password must be different from the old one',
+        );
+      default:
+        return this.usersService.update(idDto, updatePassword);
+    }
   }
 }

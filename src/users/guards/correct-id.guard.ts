7ee74430fd00b2b46b73bcc7f8fd0eda03c55dc3
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-05 22:59:51 -0400

    Create CorrectIdGuard

---
+++ src/users/guards/correct-id.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { IdDto } from '../dto/id.dto';
+
+@Injectable()
+export class CorrectIdGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const idDto = request.params as IdDto;
+    const authenticatedUserId = request.user.id;
+    const isCorrect = authenticatedUserId === idDto.id;
+    return isCorrect;
+  }
+}

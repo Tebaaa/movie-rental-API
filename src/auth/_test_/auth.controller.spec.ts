192b0b41032695a2d89005e34cf25bda3be4c9c5
Esteban Carrasco <tebacarrasco1810@gmail.com>
2022-04-06 15:01:16 -0400

    Add auth controller tests

---
+++ src/auth/_test_/auth.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from '../auth.controller';
+import { AuthService } from '../auth.service';
+
+describe('AuthController', () => {
+  let controller: AuthController;
+  const mockAuthService = {
+    login: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+  describe('login', () => {
+    const expectedReturnValue = {};
+    it('should call login service', async () => {
+      const reqObject = { user: {} };
+      mockAuthService.login.mockReturnValue(expectedReturnValue);
+      const returnedValue = await controller.login(reqObject);
+      expect(mockAuthService.login).toBeCalled();
+      expect(returnedValue).toEqual(expectedReturnValue);
+    });
+  });
+});

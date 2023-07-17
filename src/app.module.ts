884ca97c1d43a650e620eec33cdac02a896d034c
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-16 23:29:10 -0400

    🚚 Move Config directory to Core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
+import { DataSourceConfig } from '@Core/config';
 import { UsersModule } from '@Users/users.module';
 import { MoviesModule } from '@Movies/movies.module';
 import { AuthModule } from '@Auth/auth.module';
 import { AccountModule } from '@Account/account.module';
 import { MailModule } from '@Mail/mail.module';
-import { DataSourceConfig } from './config/';
 
 @Module({
   imports: [

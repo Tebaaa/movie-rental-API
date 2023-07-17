0f2c7190ccb6c22ea8ab94b65ce40064680a78b4
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-17 01:50:38 -0400

    ✨ Create movies repository

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -6,14 +6,19 @@ import { User } from '@Users/entities/';
 import { MovieEntity, RecordEntity, TagEntity } from './entities/';
 import { MoviesController } from './controllers/';
 import { MovieRentalService, MoviesService } from './services';
-import { RecordsRepository } from './repositories';
+import { MoviesRepository, RecordsRepository } from './repositories';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MovieEntity, TagEntity, RecordEntity, User]),
   ],
   controllers: [MoviesController],
-  providers: [MoviesService, MovieRentalService, RecordsRepository],
+  providers: [
+    MoviesService,
+    MovieRentalService,
+    RecordsRepository,
+    MoviesRepository,
+  ],
   exports: [MoviesService, MovieRentalService],
 })
 export class MoviesModule {}

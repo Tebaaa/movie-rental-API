cbeeb979cc2df63bec972d005a8c3f3714ce055b
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-07-16 23:25:05 -0400

    🚧 Create users repository

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -1,3 +1,14 @@
-//TODO: Create UsersRepository
+//TODO: Finish Repository
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
-export let UsersRepository;
+import { User } from '../entities';
+
+export class UsersRepository extends Repository<User> {
+  constructor(
+    @InjectRepository(User)
+    private repository: Repository<User>,
+  ) {
+    super(repository.target, repository.manager, repository.queryRunner);
+  }
+}

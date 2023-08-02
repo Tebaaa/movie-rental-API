d25d3a9d0d2005a7baa59a5f1cb8ff4676359393
Esteban Carrasco <tebacarrasco1810@gmail.com>
2023-08-02 13:25:04 -0400

    📝 Add account endpoints documentation

--- src/account/controllers/account.controller.ts
+++ src/account/controllers/account.controller.ts
@@ -8,7 +8,12 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiAcceptedResponse,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '@Auth/guards/';
 import { CorrectIdGuard } from '@Users/guards';
@@ -22,12 +27,23 @@ import { AccountService } from '../services/';
 export class AccountController {
   constructor(private accountService: AccountService) {}
 
+  //TODO: Review return type
+  @ApiOperation({
+    summary: 'Send email to reset password',
+    description:
+      'Use this endpoint to send an email to the specified account with instructions to reset its password',
+  })
   @HttpCode(HttpStatus.ACCEPTED)
   @Put('password/forgot')
   async sendEmail(@Body() emailDto: EmailDto) {
     return await this.accountService.forgotPassword(emailDto);
   }
 
+  @ApiOperation({
+    summary: 'Create a new password',
+    description: 'Use this endpoint to create a new password',
+  })
+  @ApiAcceptedResponse({ status: HttpStatus.NO_CONTENT })
   @HttpCode(HttpStatus.NO_CONTENT)
   @Put('password/reset')
   resetPassword(
@@ -37,6 +53,11 @@ export class AccountController {
     return this.accountService.resetPassword(idDto, resetPasswordDto);
   }
 
+  @ApiOperation({
+    summary: 'Change password',
+    description: 'Use this endpoint to change your old password for a new one',
+  })
+  @ApiAcceptedResponse({ status: HttpStatus.NO_CONTENT })
   @ApiBearerAuth()
   @HttpCode(HttpStatus.NO_CONTENT)
   @UseGuards(JwtAuthGuard, CorrectIdGuard)
